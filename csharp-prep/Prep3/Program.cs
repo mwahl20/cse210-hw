825a59a1b44bbbaf2cc44ffc0e0039837cc19079
CodeCrusaderlol <mwahl20@byui.edu>
2024-05-02 17:27:20 -0600

    Completed Prep3 and tried to fix DiceDistribution

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int solution = randomGenerator.Next(1,100);
+        int guess = 0;
+        int guesscount = 0;
+        string repeat = "Yes";
+        do
+        {
+            guess = 0;
+            solution = randomGenerator.Next(1,100);
+            guesscount = 0;
+            do
+            {
+                Console.Write("What is your number guess? ");
+                string response = Console.ReadLine();
+                guess = int.Parse(response);
+                if (guess > solution)
+                {
+                    Console.WriteLine("The guess is too high. Try Lower.");
+                }
+                else if (guess < solution)
+                {
+                    Console.WriteLine("The guess is too low. Please try higher.");
+                }
+                guesscount += 1;
+            }while (guess != solution);
+            Console.WriteLine($"Congratulations! You guessed the number! It was {solution}. It took you {guesscount} tries. ");
+
+            Console.Write("Would you like to continue? ");
+            repeat = Console.ReadLine();
+        }while (repeat == "yes");
     }
 }

7484d06c8c07865c6624f418bf6bb819da765351
CodeCrusaderlol <mwahl20@byui.edu>
2024-05-06 12:07:40 -0600

    Attempted to add classPractices

---
+++ sandbox/classPractices/Course.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+class Course
+{
+    public string _courseCode;
+    public string _className;
+    public int _numberof Credits;
+    public string _color;
+
+
+    //method
+    public void Display()
+    {
+        
+    }
+}

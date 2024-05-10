37132794d561377a1b042e0eb722905155a17337
CodeCrusaderlol <mwahl20@byui.edu>
2024-05-09 18:36:20 -0600

    Class Practice

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -7,7 +7,7 @@ class Program
         course1._className = "Prog. w/Classes";
         course1._color = "green";
         course1._courseCode = "CSE 201";
-        course1._numberOfCredits = 2;
+        course1._numberofCredits = 2;
         course1.Display();
     }
 }
